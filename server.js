ca625f537356f0abc5d8f601079d098caa644f35
benoellison <benoellison@gmail.com>
2022-10-11 10:21:21 -0700

    OAuth added

--- server.js
+++ server.js
@@ -4,9 +4,14 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+const session = require('express-session');
+const passport = require('passport');
+
 const methodOverride = require('method-override');
 require('dotenv').config()
+
 require('./config/database');
+require('./config/passport');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -25,6 +30,19 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(session({
+  secret: process.env.SECRET,
+  resave: false,
+  saveUninitialized: true,
+}))
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(function(req, res, next) {
+  res.locals.user = req.user;
+  next();
+})
 
 app.use(methodOverride('_method'));
 
